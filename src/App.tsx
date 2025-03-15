fc17306ec96ff7751d398502a822a84e73684612
wyltw <138242311+wyltw@users.noreply.github.com>
2025-03-15 18:43:57 +0800

    layout: set up css mesh background

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-6xl text-red-600">hello world</h1>;
+  return (
+    <main className="mesh-background min-h-screen">
+      <h1 className="text-6xl text-red-600">hello world</h1>
+    </main>
+  );
 }
 
 export default App;
