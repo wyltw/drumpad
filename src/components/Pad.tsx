fa1dc321541d671c8b9cce26e7bfc6618b62aa13
wyltw <138242311+wyltw@users.noreply.github.com>
2025-03-22 23:35:19 +0800

    layout: add animation and hover effects for pads

--- src/components/Pad.tsx
+++ src/components/Pad.tsx
@@ -3,8 +3,10 @@ type PadProps = { order: string };
 export default function Pad({ order }: PadProps) {
   return (
     <div className="flex flex-col">
-      <button className="h-36 w-36 rounded-2xl bg-zinc-800 shadow-xl" />
-      <span className="self-start">pad{order}</span>
+      <button className="hover:ring-primary/50 hover:animate-button-pulsing h-36 w-36 cursor-pointer rounded-2xl bg-zinc-800 shadow-md shadow-gray-600 transition hover:shadow-none hover:ring-4" />
+      <span className="self-start">
+        pad<span className="ms-1">{order}</span>
+      </span>
     </div>
   );
 }
