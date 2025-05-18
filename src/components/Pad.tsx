5563a5d14add7df6c21b6f1d75ae87e5753b2bc5
wyltw <138242311+wyltw@users.noreply.github.com>
2025-05-18 08:58:11 +0800

    layout: adjust hover and focus animation. feat: add more samples.

--- src/components/Pad.tsx
+++ src/components/Pad.tsx
@@ -3,7 +3,9 @@ type PadProps = { order: string };
 export default function Pad({ order }: PadProps) {
   return (
     <div className="flex flex-col">
-      <button className="hover:ring-primary/50 hover:animate-button-pulsing h-36 w-36 cursor-pointer rounded-2xl bg-zinc-800 shadow-md shadow-gray-600 transition hover:shadow-none hover:ring-4" />
+      <div className="hover:animate-button-pulsing rounded-2xl">
+        <button className="hover:ring-primary/50 focus:animate-button-pulsing h-36 w-36 cursor-pointer rounded-2xl bg-zinc-800 shadow-md shadow-gray-600 transition hover:ring-4 focus:shadow-none" />
+      </div>
       <span className="self-start">
         pad<span className="ms-1">{order}</span>
       </span>
