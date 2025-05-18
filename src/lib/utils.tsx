b418623b183cefc3d542ac95f00abf3b835f635d
wyltw <138242311+wyltw@users.noreply.github.com>
2025-05-18 11:18:57 +0800

    feat: wip in playing sounds

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -1,6 +1,12 @@
 const audioContext = new AudioContext();
+
 export const arrayBuffer = async (url: string) => {
   const sound = await fetch(url);
   const arrayBuffer = await sound.arrayBuffer();
   return audioContext.decodeAudioData(arrayBuffer);
+};
+
+const playback = async () => {
+  const playSound = audioContext.createBufferSource();
+  playSound.buffer = await arrayBuffer("");
 };
