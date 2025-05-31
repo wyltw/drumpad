3e8ca9244f93ec04f2049603a4afb44b209f6af4
wyltw <138242311+wyltw@users.noreply.github.com>
2025-05-31 13:08:55 +0800

    feat: wip. need to fix input style first next.

--- src/components/Pad.tsx
+++ src/components/Pad.tsx
@@ -2,6 +2,8 @@ import { SquarePen } from "lucide-react";
 import { Kit } from "../lib/types";
 import { playback } from "../lib/utils";
 import { Button } from "./ui/button";
+import { useState } from "react";
+import { Input } from "./ui/input";
 
 type PadProps = { order: string; sample: Kit };
 
@@ -24,12 +26,19 @@ export default function Pad({ order, sample }: PadProps) {
 type EditableTextProps = { order: string };
 
 function EditableText({ order }: EditableTextProps) {
+  const [isEditable, setIsEditable] = useState(false);
+  if (isEditable) {
+    return <Input className="" />;
+  }
   return (
     <>
       <Button
         size={"sm"}
         variant={"ghost"}
         className="group flex h-6 cursor-pointer justify-between px-1 hover:bg-transparent focus-visible:ring-0"
+        onDoubleClick={() => {
+          setIsEditable(true);
+        }}
       >
         <span className="text-sm">
           pad <span className="ms-[2px]">{order}</span>
