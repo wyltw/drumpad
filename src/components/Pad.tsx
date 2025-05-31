212a1c70575cd2974fa7786812e94ceb7a2bdd97
wyltw <138242311+wyltw@users.noreply.github.com>
2025-05-31 22:35:00 +0800

    feat: add event for input, stiil need to style input

--- src/components/Pad.tsx
+++ src/components/Pad.tsx
@@ -9,11 +9,11 @@ type PadProps = { order: string; sample: Kit };
 
 export default function Pad({ order, sample }: PadProps) {
   return (
-    <div className="flex flex-col gap-1">
+    <div className="flex w-36 flex-col gap-1">
       <div className="hover:animate-button-pulsing rounded-2xl">
         <button
           onClick={() => playback(sample.source)}
-          className="hover:ring-primary/50 focus:animate-button-pulsing h-36 w-36 cursor-pointer rounded-2xl bg-zinc-800 shadow-md shadow-gray-600 transition hover:ring-4 focus:shadow-none"
+          className="hover:ring-primary/50 focus:animate-button-pulsing size-36 cursor-pointer rounded-2xl bg-zinc-800 shadow-md shadow-gray-600 transition hover:ring-4 focus:shadow-none"
         >
           <span className="text-sm text-white">{sample.name}</span>
         </button>
@@ -27,8 +27,23 @@ type EditableTextProps = { order: string };
 
 function EditableText({ order }: EditableTextProps) {
   const [isEditable, setIsEditable] = useState(false);
+  const [value, setValue] = useState(`pad${order}`);
+  const handleBlur = (event: React.FocusEvent<HTMLInputElement, Element>) => {
+    setIsEditable(false);
+    setValue(event.target.value);
+  };
   if (isEditable) {
-    return <Input className="" />;
+    return (
+      <Input
+        value={value}
+        defaultValue={`pad${order}`}
+        onBlur={handleBlur}
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
+        className="h-6 w-full flex-1 py-0 font-sans md:text-base"
+      />
+    );
   }
   return (
     <>
