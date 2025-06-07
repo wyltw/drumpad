c55afcf7c681f2e84a39f41978889fea565cf4be
wyltw <138242311+wyltw@users.noreply.github.com>
2025-06-07 17:12:15 +0800

    fix: remove useless w-36

--- src/components/Pad.tsx
+++ src/components/Pad.tsx
@@ -6,10 +6,12 @@ type PadProps = { order: string; sample: Kit };
 
 export default function Pad({ order, sample }: PadProps) {
   return (
-    <div className="flex w-36 flex-col gap-1">
+    <div className="flex flex-col gap-1">
       <div className="hover:animate-button-pulsing rounded-2xl">
         <button
-          onClick={() => playback(sample.source)}
+          onClick={() => {
+            playback(sample.source);
+          }}
           className="hover:ring-primary/50 focus:animate-button-pulsing size-36 cursor-pointer rounded-2xl bg-zinc-800 shadow-md shadow-gray-600 transition hover:ring-4 focus:shadow-none"
         >
           <span className="text-sm text-white">{sample.name}</span>
