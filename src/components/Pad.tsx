71c35927ef87db109783e5518b7e952e0ff8400c
wyltw <138242311+wyltw@users.noreply.github.com>
2025-06-24 23:26:27 +0800

    layout: try to add more effect for animation

--- src/components/Pad.tsx
+++ src/components/Pad.tsx
@@ -14,7 +14,7 @@ export default function Pad({ order, sample }: PadProps) {
         className="hover:ring-primary/75 relative size-32 cursor-pointer rounded-2xl bg-zinc-800 shadow-md shadow-gray-600 transition hover:ring-4 focus:shadow-none"
       >
         <span className="text-sm text-white">{sample.name}</span>
-        <span className="ring-primary/75 hover:animate-button-pulsing absolute top-3/6 left-3/6 block size-32 -translate-16 scale-100 rounded-2xl opacity-100 hover:ring-2"></span>
+        <span className="ring-primary/75 hover:animate-button-pulsing absolute top-3/6 left-3/6 block size-32 -translate-16 scale-100 rounded-2xl opacity-100 ease-in-out hover:ring-2"></span>
       </button>
       <EditableText order={order} name={sample.name} />
     </div>
