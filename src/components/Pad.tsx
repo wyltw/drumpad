2026b30b14aa23048892ae5fe9100e803e6672f2
wyltw <wyltw812@gmail.com>
2025-06-25 15:58:59 +0800

    fix: misapply transtion related property for aniamtion. layout: check class usage. refactor: refactor for better jsx structure.

--- src/components/Pad.tsx
+++ src/components/Pad.tsx
@@ -1,3 +1,4 @@
+import React, { ComponentPropsWithoutRef, ReactNode } from "react";
 import { Kit } from "../lib/types";
 import { playback } from "../lib/utils";
 import { EditableText } from "./EditableText";
@@ -5,18 +6,40 @@ import { EditableText } from "./EditableText";
 type PadProps = { order: string; sample: Kit };
 
 export default function Pad({ order, sample }: PadProps) {
+  const handleClick = () => {
+    playback(sample.source);
+  };
+
   return (
     <div className="flex flex-col gap-1">
-      <button
-        onClick={() => {
-          playback(sample.source);
-        }}
-        className="hover:ring-primary/75 relative size-32 cursor-pointer rounded-2xl bg-zinc-800 shadow-md shadow-gray-600 transition hover:ring-4 focus:shadow-none"
-      >
+      <PadButton onClick={handleClick}>
         <span className="text-sm text-white">{sample.name}</span>
-        <span className="ring-primary/75 hover:animate-button-pulsing absolute top-3/6 left-3/6 block size-32 -translate-16 scale-100 rounded-2xl opacity-100 ease-in-out hover:ring-2"></span>
-      </button>
+        <Mask />
+      </PadButton>
       <EditableText order={order} name={sample.name} />
     </div>
+  );
+}
+
+type PadButtonProps = {
+  children: ReactNode;
+} & ComponentPropsWithoutRef<"button">;
+
+function PadButton({ children, onClick }: PadButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="ring-primary/75 relative size-32 cursor-pointer rounded-2xl bg-zinc-800 shadow-md shadow-gray-600 transition hover:ring-4 focus:shadow-none"
+      // hover時出現ring-4，focus時取消box-shadow
+    >
+      {children}
+    </button>
+  );
+}
+
+function Mask() {
+  return (
+    <span className="ring-primary/75 hover:animate-button-pulsing absolute top-3/6 left-3/6 block size-32 -translate-16 scale-100 rounded-2xl opacity-100 ring-2" />
+    // hover時播放動畫，scale變大，opacity變淡，常時ring-2
   );
 }
