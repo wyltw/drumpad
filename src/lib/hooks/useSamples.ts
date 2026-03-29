092ce8f36ce221ba56105ddb4affcfe1054c2687
wyltw <wyltw812@gmail.com>
2026-03-29 20:39:03 +0800

    refactor: useQuery implement for useSamples

--- src/lib/hooks/useSamples.ts
+++ src/lib/hooks/useSamples.ts
@@ -1,30 +1,22 @@
-import { useCallback, useEffect, useState, useTransition } from "react";
-import { decodeSample, loadSample } from "../utils/utils";
+import { useQuery } from "@tanstack/react-query";
 import { HOUSE_KIT } from "../constants";
-import { SampleDecoded } from "../types/types";
+import { decodeSample, loadSample } from "../utils/utils";
 import { toast } from "sonner";
 
 export const useSamples = (selectedKit: string, audioContext: AudioContext) => {
-  const [samples, setSamples] = useState<SampleDecoded[] | null>(null);
-  const [isPending, startTransiion] = useTransition();
-
-  const loadSamples = useCallback(async () => {
-    startTransiion(async () => {
+  const { data, isPending } = useQuery({
+    queryKey: ["samples", selectedKit],
+    queryFn: () => {
       const samplePromise = decodeSample(loadSample(HOUSE_KIT), audioContext);
+
       toast.promise(samplePromise, {
-        loading: "音源載入中...",
-        success: "載入完成",
+        loading: "Loading samples...",
+        success: "Samples loaded",
       });
-      const sample = await samplePromise;
-      setSamples(sample);
-    });
-  }, [audioContext]);
 
-  useEffect(() => {
-    if (selectedKit === "default") {
-      loadSamples();
-    }
-  }, [selectedKit, loadSamples]);
+      return samplePromise;
+    },
+  });
 
-  return { samples, isPending };
+  return { samples: data ?? null, isPending };
 };
