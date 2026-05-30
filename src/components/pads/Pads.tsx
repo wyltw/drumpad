600896ea172487bc4a6d590bc5c864fef2d7aaf9
wyltw <wyltw812@gmail.com>
2026-05-30 11:40:35 +0800

    layout: add spinner

--- src/components/pads/Pads.tsx
+++ src/components/pads/Pads.tsx
@@ -3,6 +3,8 @@ import Pad from "./Pad";
 import { useKit } from "@/lib/adapters/KitsAdapter";
 import PadsToolbar from "./PadsToolbar";
 import VolumeContextProvider from "@/lib/contexts/VolumeContextProvider";
+import { Spinner } from "../ui/spinner";
+import { ReactNode } from "react";
 
 export default function Pads() {
   const { selectedKit } = useKitContext();
@@ -11,14 +13,28 @@ export default function Pads() {
 
   return (
     <VolumeContextProvider>
-      <div className="mt-4 flex gap-2">
-        <div className="grid grid-cols-3 grid-rows-3 place-items-center gap-x-10 gap-y-4">
-          {pads.map((pad) => (
-            <Pad key={pad.id} pad={pad} />
-          ))}
-        </div>
-        <PadsToolbar pads={pads} />
+      <div className="mt-4 flex justify-center gap-2">
+        {kit ? (
+          <>
+            <PadsLayout>
+              {pads.map((pad) => (
+                <Pad key={pad.id} pad={pad} />
+              ))}
+            </PadsLayout>
+            <PadsToolbar pads={pads} />
+          </>
+        ) : (
+          <Spinner className="size-32" />
+        )}
       </div>
     </VolumeContextProvider>
+  );
+}
+
+function PadsLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="grid grid-cols-3 grid-rows-3 place-items-center gap-x-10 gap-y-4">
+      {children}
+    </div>
   );
 }
